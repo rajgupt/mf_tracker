cc7c19c9af25a42f673f064b4cd888366d15bf5f
rajgupt <rajat.y6@gmail.com>
2017-02-26 00:38:55 +0530

    nav viewer ui file

---
+++ mf_tracker_ui.py
@@ -0,0 +1,34 @@
+import mf_nav_ui
+import nav
+import exceptions
+from PySide.QtCore import *
+from PySide.QtGui import *
+import mf_tracker
+
+TimeIntervalInYears = 1
+
+class NAVWindow(QMainWindow, mf_nav_ui.Ui_MainWindow):
+    def __init__(self, *args):
+        super(NAVWindow, self).__init__(*args)
+        self.setupUi(self)
+        self.startDateEdit.setDate(QDate.currentDate().addYears(-1*TimeIntervalInYears))
+        self.endDateEdit.setDate(QDate.currentDate())
+
+        self.navWidget = QWidget(self)
+        self.verticalLayout.addWidget(self.navWidget)
+        l = QVBoxLayout(self.navWidget)
+        self.navPlotCanvas = nav.NavCanvas(self.navWidget)
+        l.addWidget(self.navPlotCanvas)
+        self.connect(self.comboBox, SIGNAL("currentIndexChanged(int)"), self.mf_selected)
+        self.connect(self.plotNavButton, SIGNAL("clicked()"), self.plotNavCallback)
+        
+
+    def mf_selected(self, int):
+        pass
+
+    def plotNavCallback(self):
+        qdateStart = self.startDateEdit.date()
+        qdateEnd = self.endDateEdit.date()
+        dateStart = str(qdateStart.year())+"/"+str(qdateStart.month())+"/"+str(qdateStart.day())
+        dateEnd = str(qdateEnd.year())+"/"+str(qdateEnd.month())+"/"+str(qdateEnd.day())
+        nav.plotNav(self.navPlotCanvas, self.comboBox.currentText(), dateStart, dateEnd)
